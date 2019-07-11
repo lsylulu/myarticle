55029ae12ee0e342ed4469997739d2284869036a
shengye.lv <739352820@qq.com>
2019-07-11 13:36:19 +0800

    2019.6.5-花点时间彻底搞懂快排

--- src/main/java/quicksort/ClassicalQuickSort.java
+++ src/main/java/quicksort/ClassicalQuickSort.java
@@ -2,72 +2,68 @@ package quicksort;
 
 public class ClassicalQuickSort {
 
-    public static void quickSort(int[] arr){
+    public static void quickSort(int[] arr) {
 
         if (arr == null || arr.length < 2) {
             return;
         }
         //开启快速排序
-        quickSort(arr,0,arr.length-1);
+        quickSort(arr, 0, arr.length - 1);
     }
 
-    public static void quickSort(int[] arr,int l,int r){
-
-        if(l<r){
-            int cur=partition(arr,l,r);
+    public static void quickSort(int[] arr, int start, int end) {
+        if (start < end) {
+            //基准数的索引
+            int cur = partition(arr, start, end);
             //排序cur左边的
-            quickSort(arr,l,cur-1);
+            quickSort(arr, start, cur - 1);
             //排序cur右边的
-            quickSort(arr,cur+1,r);
+            quickSort(arr, cur + 1, end);
         }
-
     }
 
     /**
-     * 一趟排序，以r为基准数，给l-r上的数排序
-     * 小于等于r的方左边；大于r的放右边
-     * 保证more和more的右边的数大于arr[r]
+     * 一趟排序，以arr[left]为基准数，小于等于它的放左边，大于等于它的放右边
      * @param arr
-     * @param l
-     * @param r
+     * @param left
+     * @param right
      * @return
      */
-    public static int partition(int[] arr,int l,int r){
+    public static int partition(int[] arr, int left, int right) {
 
-        //当前数组的右边界
-        int more=r+1;
-        int num=arr[r];
-        while (l<more){
+        //记录基准数
+        int pivot = arr[left];
+        while (left < right) {
 
-            if(arr[l]<=num){
-                l++;
-            }else{
-                //--more，保证每一次排序，从more开始一定是大于num的
-                //交换后并不知道arr[l]>x还是arr[l]<=x
-                //因此l还是在原地
-                swap(arr,l,--more);
+            //缩小右边界
+            while (left < right && arr[right] >= pivot) {
+                right--;
+            }
+            //右边的小数顶掉左边的大数
+            if (left < right) {
+                arr[left++] = arr[right];
+            }
+
+            //缩小左边界
+            while (left < right && arr[left] <= pivot) {
+                left++;
+            }
+            //左边的大数顶掉右边的小数
+            if (left < right) {
+                arr[right--] = arr[left];
             }
         }
-//        if(num>=arr[more]){
-//            return r;
-//        }
-//        swap(arr,more-1,r);
-        //more-1必然是最终基准数所在的索引
-        return more-1;
-    }
-
-    public static void swap(int[] arr, int i, int j) {
-        int tmp = arr[i];
-        arr[i] = arr[j];
-        arr[j] = tmp;
+        //最终left的所在被基准数顶替，返回基准数
+        arr[left] = pivot;
+        return left;
     }
 
     public static void main(String[] args) {
-        int[] arr={1,3,4,6,9};
+        int[] arr = {3, 4, 2, 77, 5, 34, 0, 8};
         quickSort(arr);
         System.out.println();
         for (int i : arr) {
-            System.out.print(i+" ");
+            System.out.print(i + " ");
         }
     }
 }
