11a41953f36af0d01ea3f02a4b842ed4e7a10889
shengye.lv <739352820@qq.com>
2019-07-12 14:54:52 +0800

    2019.6.5-花点时间彻底搞懂快排

--- src/main/java/quicksort/ClassicalQuickSort.java
+++ src/main/java/quicksort/ClassicalQuickSort.java
@@ -11,14 +11,14 @@ public class ClassicalQuickSort {
         quickSort(arr, 0, arr.length - 1);
     }
 
-    public static void quickSort(int[] arr, int start, int end) {
-        if (start < end) {
+    public static void quickSort(int[] arr, int left, int right) {
+        if (left < right) {
             //基准数的索引
-            int cur = partition(arr, start, end);
+            int cur = partition(arr, left, right);
             //排序cur左边的
-            quickSort(arr, start, cur - 1);
+            quickSort(arr, left, cur - 1);
             //排序cur右边的
-            quickSort(arr, cur + 1, end);
+            quickSort(arr, cur + 1, right);
         }
     }
 
@@ -32,8 +32,17 @@ public class ClassicalQuickSort {
     public static int partition(int[] arr, int left, int right) {
 
         //记录基准数
-        int pivot = arr[left];
+        int pivot = arr[right];
         while (left < right) {
+
+            //缩小左边界：基准数在右边，必须从左边开始找
+            while (left < right && arr[left] <= pivot) {
+                left++;
+            }
+            //左边的大数顶掉右边的小数
+            if (left < right) {
+                arr[right--] = arr[left];
+            }
 
             //缩小右边界
             while (left < right && arr[right] >= pivot) {
@@ -44,22 +53,14 @@ public class ClassicalQuickSort {
                 arr[left++] = arr[right];
             }
 
-            //缩小左边界
-            while (left < right && arr[left] <= pivot) {
-                left++;
-            }
-            //左边的大数顶掉右边的小数
-            if (left < right) {
-                arr[right--] = arr[left];
-            }
         }
         //最终left的所在被基准数顶替，返回基准数
-        arr[left] = pivot;
-        return left;
+        arr[right] = pivot;
+        return right;
     }
 
     public static void main(String[] args) {
-        int[] arr = {3, 4, 2, 77, 5, 34, 0, 8};
+        int[] arr = {3, 2, 6, 5, 0, 7, 9};
         quickSort(arr);
         System.out.println();
         for (int i : arr) {
